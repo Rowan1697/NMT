ee00f56c353bdeba7d2753d35e52173973f09c9b
rowan <rowanhossain@gmail.com>
2021-03-10 02:59:13 +0600

    add sh file

---
+++ model.sh
@@ -0,0 +1,7 @@
+pip install gdown
+mkdir model
+cd model
+gdown https://drive.google.com/uc?id=1okPd4ifQQuWrEH5hiz6ikX8G8EBoGVOk
+gdown https://drive.google.com/uc?id=1_djURBJCIToETfTXCRrX2N5gN1qRukHe
+gdown https://drive.google.com/uc?id=15wjG2d09DAw8P7kU09oM4UfRplkpRKYU
+gdown https://drive.google.com/uc?id=1TFbXFSUhGTb9aopgssM6Hn86Cyoh-38K
